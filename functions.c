8e9dc25129a3c797f51598660ccc2ecc44605418
wujingSa <samojuade1@gmail.com>
2023-10-17 15:58:51 -0700

    compilation updated

--- functions.c
+++ functions.c
@@ -12,11 +12,11 @@ int print_char(va_list ap, params_t *params)
 	unsigned int pad = 1, sum = 0, ch = va_arg(ap, int);
 
 	if (params->minus_flag)
-		sum += _putchar(ch);
+	sum += _putchar(ch);
 	while (pad++ < params->width)
-		sum += _putchar(pad_char);
+	sum += _putchar(pad_char);
 	if (!params->minus_flag)
-		sum += _putchar(ch);
+	sum += _putchar(ch);
 	return (sum);
 }
 
@@ -31,11 +31,11 @@ int print_int(va_list ap, params_t *params)
 	long l;
 
 	if (params->l_modifier)
-		l = va_arg(ap, long);
+	l = va_arg(ap, long);
 	else if (params->h_modifier)
-		l = (short int)va_arg(ap, int);
+	l = (short int)va_arg(ap, int);
 	else
-		l = (int)va_arg(ap, int);
+	l = (int)va_arg(ap, int);
 	return (print_number(convert(l, 10, 0, params), params));
 }
 
@@ -47,35 +47,34 @@ int print_int(va_list ap, params_t *params)
  */
 int print_string(va_list ap, params_t *params)
 {
-	char *str = va_arg(ap, char *), pad_char = ' ';
+	char *str = va_arg(ap, char *);
+	char pad_char = ' ';
 	unsigned int pad = 0, sum = 0, i = 0, j;
 
-	(void)params;
-	switch ((int)(!str))
-		case 1:
-			str = NULL_STRING;
+	if (!str)
+	str = NULL_STRING;
 
 	j = pad = _strlen(str);
 	if (params->precision < pad)
-		j = pad = params->precision;
+	j = pad = params->precision;
 
 	if (params->minus_flag)
 	{
-		if (params->precision != UINT_MAX)
-			for (i = 0; i < pad; i++)
-				sum += _putchar(*str++);
-		else
-			sum += _puts(str);
+	if (params->precision != UINT_MAX)
+	for (i = 0; i < pad; i++)
+		sum += _putchar(*str++);
+	else
+		sum += _puts(str);
 	}
 	while (j++ < params->width)
-		sum += _putchar(pad_char);
+	sum += _putchar(pad_char);
 	if (!params->minus_flag)
 	{
-		if (params->precision != UINT_MAX)
-			for (i = 0; i < pad; i++)
-				sum += _putchar(*str++);
-		else
-			sum += _puts(str);
+	if (params->precision != UINT_MAX)
+	for (i = 0; i < pad; i++)
+		sum += _putchar(*str++);
+	else
+		sum += _puts(str);
 	}
 	return (sum);
 }
@@ -105,23 +104,26 @@ int print_S(va_list ap, params_t *params)
 	char *hex;
 	int sum = 0;
 
-	if ((int)(!str))
-		return (_puts(NULL_STRING));
+	if (!str)
+	return (_puts(NULL_STRING));
 	for (; *str; str++)
 	{
-		if ((*str > 0 && *str < 32) || *str >= 127)
-		{
-			sum += _putchar('\\');
-			sum += _putchar('x');
-			hex = convert(*str, 16, 0, params);
-			if (!hex[1])
-				sum += _putchar('0');
-			sum += _puts(hex);
-		}
-		else
-		{
-			sum += _putchar(*str);
-		}
+	if ((*str > 0 && *str < 32) || *str >= 127)
+	{
+		sum += _putchar('\\');
+		sum += _putchar('x');
+		hex = convert(*str, 16, 0, params);
+	if (!hex[1])
+	{
+		sum += _putchar('0');
+	}
+		sum += _puts(hex);
+		free(hex);
+	}
+	else
+	{
+		sum += _putchar(*str);
+	}
 	}
 	return (sum);
 }
